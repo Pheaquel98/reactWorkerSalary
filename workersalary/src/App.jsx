8b3dd116266bf4f153bc72cc02c3c13756a9af9e
pheaquel98 <gallactofener123@hotmail.com>
2023-04-11 09:38:01 +0300

    ...

--- workersalary/src/App.jsx
+++ workersalary/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react"
 import "./App.css"
+import AddWorker from "./components/Workers/AddWorker"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <AddWorker />
     </div>
   )
 }
---
+++ workersalary/src/components/Workers/AddWorker.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const AddWorker = () => {
+  return <div>AddWorker</div>
+}
+
+export default AddWorker
