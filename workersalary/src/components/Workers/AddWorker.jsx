5827e81bae86bb1e635d54b745f7a9415ebd7764
pheaquel98 <gallactofener123@hotmail.com>
2023-04-11 09:56:31 +0300

    card component created

--- workersalary/src/components/Workers/AddWorker.jsx
+++ workersalary/src/components/Workers/AddWorker.jsx
@@ -1,28 +1,33 @@
 import React from "react"
+import Card from "../UI/Card"
 
 const AddWorker = () => {
   return (
-    <form className="flex flex-col gap-y-2">
-      <label htmlFor="name" className="font-medium">
-        Çalışan İsmi
-      </label>
-      <input
-        type="text"
-        className="max-w-[40rem] w-full mx-auto border p-2"
-        placeholder="Çalışan ismi yazınız"
-        id="name"
-      />
-      <label htmlFor="salary" className="font-medium">
-        Maaş Miktarı
-      </label>
-      <input
-        type="text"
-        className="max-w-[40rem] w-full mx-auto border p-2"
-        placeholder="Maaş miktarı yazınız"
-        id="salary"
-      />
-      <button className="p-2 text-lg bg-teal-700 text-white mt-2">Ekle</button>
-    </form>
+    <Card className="mt-10">
+      <form className="flex flex-col gap-y-2">
+        <label htmlFor="name" className="font-medium">
+          Çalışan İsmi
+        </label>
+        <input
+          type="text"
+          className="max-w-[40rem] w-full mx-auto border p-2"
+          placeholder="Çalışan ismi yazınız"
+          id="name"
+        />
+        <label htmlFor="salary" className="font-medium">
+          Maaş Miktarı
+        </label>
+        <input
+          type="number"
+          className="max-w-[40rem] w-full mx-auto border p-2"
+          placeholder="Maaş miktarı yazınız"
+          id="salary"
+        />
+        <button className="p-2 text-lg bg-teal-700 text-white mt-2">
+          Ekle
+        </button>
+      </form>
+    </Card>
   )
 }
 
